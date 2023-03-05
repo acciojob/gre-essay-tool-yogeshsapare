2b98a7edd1bc0c52401388ce95cb863fcdc720c9
yogeshsapare <117711304+yogeshsapare@users.noreply.github.com>
2023-03-06 00:00:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+const textElement = document.getElementById('evaluatedText')
+const h3Element = document.getElementById('wordCount')
+
+function handelInput(e){
+  const sentence = e.target.value
+	let match=0;
+	if(sentence.trim()===""){
+		match=0;
+	}else{
+		match = sentence.match(/\S+/ig).length;
+	}
+  h3Element.textContent = match;
+}
+textElement.addEventListener('input',handelInput)
